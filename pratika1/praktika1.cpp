c15f5d9c29ee90db44ea28d28e6633e8cdd8f1df
Tobias Grothusmann <tobias.grothusmann@gmx.de>
2015-10-13 15:22:55 +0200

    initial commit

---
+++ pratika1/praktika1.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tobias on 13.10.2015.
+//
+
+#include "praktika1.h"
---
+++ pratika1/praktika1.h
@@ -0,0 +1,11 @@
+//
+// Created by Tobias on 13.10.2015.
+//
+
+#ifndef EPR1516REPO_C_PRAKTIKA1_H
+#define EPR1516REPO_C_PRAKTIKA1_H
+
+#endif //EPR1516REPO_C_PRAKTIKA1_H
+void main(){
+
+}
