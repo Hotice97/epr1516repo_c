0c220113806e56294e956962171d65f6e55c892c
Tobias Grothusmann <tobias.grothusmann@gmx.de>
2015-10-27 21:48:40 +0100

    Add algorithm as C file

---
+++ pratika1/c_praktika1.c
@@ -0,0 +1,41 @@
+//
+// Created by Tobias on 27.10.2015.
+//
+
+#include "c_praktika1.h"
+
+int schaltjahr(int jahr){
+    if(jahr%4==0 && jahr!=100 || jahr%400==0){
+        printf("Das gewaehlte Jahr ist ein Schaltjahr");
+    } else {
+        printf("Das gewaehlte Jahr ist kein Schaltjahr");
+    }
+}
+
+int jahresliste(int anfangsjahr, int endjahr){
+    do{
+        if(anfangsjahr%4==0 && anfangsjahr!=100 || anfangsjahr%400==0){
+            printf("%d",anfangsjahr);
+            printf(": Ist ein Schaltjahr\n");
+        } else {
+            printf("%d",anfangsjahr);
+            printf(": Ist kein Schaltjahr\n");
+        }
+        anfangsjahr ++;
+    }while(anfangsjahr<=endjahr);
+    return 0;
+}
+
+int main(){
+    int eingabe;
+    int eingabe2;
+    printf("Gib ein Jahr deiner Wahl ein: ");
+    scanf("%d",&eingabe);
+    schaltjahr(eingabe);
+    printf("\nGib das Anfangsjahr ein: ");
+    scanf("%d",&eingabe);
+    printf("Gib das Endjahr ein: ");
+    scanf("%d",&eingabe2);
+    jahresliste(eingabe,eingabe2);
+    return 0;
+}
---
+++ pratika1/c_praktika1.h
@@ -0,0 +1,8 @@
+//
+// Created by Tobias on 27.10.2015.
+//
+
+#ifndef EPR1516REPO_C_C_PRAKTIKA1_H
+#define EPR1516REPO_C_C_PRAKTIKA1_H
+#include <stdio.h>
+#endif //EPR1516REPO_C_C_PRAKTIKA1_H
