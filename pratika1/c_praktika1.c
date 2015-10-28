ecdfe4c41ca1551ae76429690710835bc95d6333
Tobias Grothusmann <tobias.grothusmann@gmx.de>
2015-10-28 14:17:35 +0100

    Added Brackets to if statement to get right result

--- pratika1/c_praktika1.c
+++ pratika1/c_praktika1.c
@@ -5,7 +5,7 @@
 #include "c_praktika1.h"
 
 int schaltjahr(int jahr){
-    if( jahr%400==0 || jahr%4==0 && jahr%100!=0){
+    if( jahr%400==0 || (jahr%4==0 && jahr%100!=0) ){
         printf("Das gewaehlte Jahr ist ein Schaltjahr");
     } else {
         printf("Das gewaehlte Jahr ist kein Schaltjahr");
@@ -14,7 +14,7 @@ int schaltjahr(int jahr){
 
 int jahresliste(int anfangsjahr, int endjahr){
     do{
-        if( anfangsjahr%400==0 || anfangsjahr%4==0 && anfangsjahr%100!=0){
+        if( anfangsjahr%400==0 || (anfangsjahr%4==0 && anfangsjahr%100!=0) ){
             printf("%d",anfangsjahr);
             printf(": Ist ein Schaltjahr\n");
         } else {
@@ -29,13 +29,17 @@ int jahresliste(int anfangsjahr, int endjahr){
 int main(){
     int eingabe;
     int eingabe2;
+
     printf("Gib ein Jahr deiner Wahl ein: ");
     scanf("%d",&eingabe);
+
     schaltjahr(eingabe);
+
     printf("\nGib das Anfangsjahr ein: ");
     scanf("%d",&eingabe);
     printf("Gib das Endjahr ein: ");
     scanf("%d",&eingabe2);
+
     jahresliste(eingabe,eingabe2);
     return 0;
 }
