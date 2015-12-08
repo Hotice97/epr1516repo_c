af0581d050e55d1f1c27dcdb0a0722257622d8dd
Tobias Grothusmann <tobias.grothusmann@gmx.de>
2015-12-09 00:58:01 +0100

    Praktika 4

---
+++ pratika1/martikelnummern.c
@@ -0,0 +1,118 @@
+//
+// Created by Tobias on 08.12.2015.
+//
+#include <stdio.h>
+#include <stdbool.h>
+
+int anzeigen(int martikelnummern[], int arraylength){
+    for(int i = 0; i < arraylength;i++){
+        printf("%d ,",martikelnummern[i]);
+    }
+    printf("\n");
+}
+
+int finde_index(int martikelnummern[], int arraylength, int value) {
+    int i;
+    for (i=0; i<arraylength; i++)
+    {
+        if (martikelnummern[i] == value) {
+            return i;
+        }
+    }
+    return(-1);
+}
+
+
+int suchen(int maritkelnummer[], int value, int arraylength){
+
+    int index;
+    index = finde_index(maritkelnummer, arraylength, value);
+    if (index == -1) {
+        printf("Die gesuchte Martikelnummer %d konnte nicht gefunden werden.\n", value);
+    } else {
+        printf("Die gesuchte Martikelnummer %d wurde an der Stelle %d gefunden\n", value, index);
+    }
+}
+
+bool vorhanden(int maritkelnummer[], int value, int arraylength){
+
+    int index;
+    index = finde_index(maritkelnummer, arraylength, value);
+    if (index == -1) {
+        false;
+    } else {
+        true;
+    }
+}
+
+int hinzufuegen(int martikelnummern[],int stelle, int value){
+
+    martikelnummern[stelle] = value;
+}
+
+int loeschen(int marikelnummern[],int position,int n){
+//    for(int i = position+1; i < n;i++){
+//        marikelnummern[i - 1] = marikelnummern[i];
+//    }
+//    n++;
+}
+
+
+int main(void) {
+    int eingabe,eingabe3,eingabe4 = 0,eingabe5 = 0,eingabe6 = 0;
+
+    printf("Wie gross soll das Array aus Martikelnummer sein? ");
+    scanf("%d",&eingabe4);
+    int arraylength = eingabe4 ;
+    int arraystelle = 0;
+
+    int martikelnummern[arraylength];
+
+
+    while(true) {
+        eingabe = 0;
+        printf("Sie befinden sich im Hauptmenue, was moechten sie tun? (1 = Alle Martikelnummer anzeigen , 2 = Nach einer Martikelnummer suchen, 3 = Hinzufuegen einer Martikelnumer , 4 = Loeschen einer Martikelnumer");
+        scanf("%d", &eingabe);
+
+        switch (eingabe){
+            case 1: anzeigen(martikelnummern,arraylength);
+                break;
+
+            case 2:
+                eingabe3 = 0;
+                printf("Nach welcher Martikelnummer moechten sie suchen? ");
+                scanf("%d",&eingabe3);
+                suchen(martikelnummern,eingabe3,arraylength);
+                break;
+
+            case 3:eingabe5 = 0;
+                printf("Welche Martikelnummer moechten sie hinzufuegen? ");
+                scanf("%d",&eingabe5);
+                if(!vorhanden(martikelnummern,eingabe5,arraylength)){
+                    if(arraystelle<arraylength){
+                        hinzufuegen(martikelnummern, arraystelle, eingabe5);
+                        arraystelle++;
+                        printf("Die Martikelnummer wurde erfolgreich hinzugefügt!\n");
+                    } else {
+                        printf("Es können keine weiteren Martikelnummer hinzugefügt werden!\n");
+                    }
+                } else{
+                    printf("Die gewählte Martikelnummer ist bereits vorhanden\n");
+                }
+                break;
+
+            case 4:
+                eingabe6 = 0;
+                printf("Welche Martikelnummer wollen sie entfernen? ");
+                scanf("%d",&eingabe6);
+                if(finde_index(martikelnummern,arraylength,eingabe6)!=-1){
+                    int index = finde_index(martikelnummern,arraylength,eingabe6);
+                    loeschen(martikelnummern,index,arraylength);
+                    printf("Erfolgreich entfernt\n");
+                 } else{
+                    printf("Entfernen fehlgeschlagen!\n");
+                }
+                break;
+        }
+    }
+}
---
+++ pratika1/praktika4.c
@@ -0,0 +1,81 @@
+//
+// Created by Tobias on 08.12.2015.
+//
+#include <stdio.h>
+int main(void) {
+
+    printf("Beachte bei der Eingabe, dass beide Matrizen gleich groß sind, d.h. gleich Anzahl an Spalten und Zeilen haben! Ansonsten können diese nicht addiert werden \n\n");
+
+    //Matrix A
+    unsigned int spalten1,zeilen1;
+    float eingabe1 = 0;
+    printf("Gib die Anzahl der Spalten ein: ");
+    scanf("%d", &spalten1);
+    printf("Gib die Anzahl der Zeilen ein: ");
+    scanf("%d", &zeilen1);
+
+    float matrix_a[zeilen1*spalten1];
+
+    for(int i = 0; i<zeilen1*spalten1;i++){
+        printf("Gib eine Zahl ein: ");
+        scanf("%f", &eingabe1);
+        matrix_a[i] = eingabe1;
+        eingabe1 = 0;
+    }
+    int m = 0;
+    printf("Ihre eingegebene Matrix lautet: \n\n");
+    for(int j = sizeof(matrix_a)/ sizeof(matrix_a[0]);j>0;j-- ){
+
+        printf("%10.3f ", matrix_a[m]);
+        m ++;
+        if(m%spalten1==0) {
+            printf("\n");
+        }
+
+    }
+
+
+    //Matrix B
+    unsigned int spalten2,zeilen2;
+    float eingabe2 = 0;
+    printf("Gib die Anzahl der Spalten ein: ");
+    scanf("%d", &spalten2);
+    printf("Gib die Anzahl der Zeilen ein: ");
+    scanf("%d", &zeilen2);
+
+    float matrix_b[zeilen2*spalten2];
+
+    for(int k = 0; k<zeilen2*spalten2;k++){
+        printf("Gib eine Zahl ein: ");
+        scanf("%f", &eingabe2);
+        matrix_b[k] = eingabe2;
+        eingabe2 = 0;
+    }
+    int b = 0;
+    printf("Ihre eingegebene Matrix lautet: \n\n");
+    for(int l = sizeof(matrix_b)/ sizeof(matrix_b[0]);l>0;l-- ){
+
+        printf("%10.3f ", matrix_b[b]);
+        b ++;
+        if(b%spalten2==0) {
+            printf("\n");
+        }
+
+    }
+
+    int e = 0;
+    float matrix_c[sizeof(matrix_b)/ sizeof(matrix_b[0])];
+    //Addition
+    printf("Die Matrix C die aus der Addition von Matrix A und Matrix B entstanden ist, lautet:\n\n ");
+    for(int n = sizeof(matrix_b)/ sizeof(matrix_b[0]);n>0;n-- ){
+
+        matrix_c[e] = matrix_a[e] + matrix_b[e];
+        printf("%10.3f ", matrix_c[e]);
+        e ++;
+        if(e%spalten2==0) {
+            printf("\n");
+        }
+
+    }
+
+}
